00f515f29478b8d6f82dcc4603d704d96d990e43
Marcel Joshua <marceljoshua69@gmail.com>
2024-11-15 15:04:22 +0700

    initial push

---
+++ src/main/java/com/ptsisi/vendorapi/PtsisiVendorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ptsisi.vendorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtsisiVendorApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PtsisiVendorApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ptsisi/vendorapi/PtsisiVendorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptsisi.vendorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtsisiVendorApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
